d94e519a4dd2cb041ea12336bcf25a99741175c3
Pedrohcamposf <53444454+Pedrohcamposf@users.noreply.github.com>
2023-04-04 16:43:03 -0300

    ListaATP

---
+++ ListasATP/ListasATP/SelecaoLista.cs
@@ -0,0 +1,28 @@
+﻿using ListaATP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaATP
+{
+    public static class Lista
+    {
+        public static void Executar()
+        {
+            int resposta;
+            Console.WriteLine("Entre com o numero da questao");
+            resposta = int.Parse(Console.ReadLine());
+            switch (resposta)
+            {
+                case 4:
+                    Questao4.Executar();
+                    break;
+                case 5:
+                    Questao5.Executar();
+                    break;
+            }
+        }
+    }
+}
